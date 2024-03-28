1af1993e36da161cf136cdde27481d21e820d4ec
NikolaiGTR <nikolai.olekhnovitch@polymtl.ca>
2024-03-28 17:18:32 -0400

    TessEval and Scenes completed

--- shaders/tessellation.tcs.glsl
+++ shaders/tessellation.tcs.glsl
@@ -11,21 +11,46 @@ const float MAX_TESS = 64;
 const float MIN_DIST = 30.0f;
 const float MAX_DIST = 100.0f;
 
+float calcTess(vec4 centerPoint) {
+	return mix(MAX_TESS, MIN_TESS, (clamp(length(centerPoint.xyz), MIN_DIST, MAX_DIST) - MIN_DIST) / (MAX_DIST - MIN_DIST));
+}
+
+// mix(MAX_TESS, MIN_TESS, (clamp(length(centerOL0.xyz), MIN_DIST, MAX_DIST) - MIN_DIST)/(MAX_DIST - MIN_DIST)
+
 void main()
 {
 	// TODO
 
-    gl_in[0].gl_Position; // (0,0)
-    gl_in[1].gl_Position; // (1,0)
-    gl_in[2].gl_Position; // (1,1)
-    gl_in[3].gl_Position; // (0,1)
+    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position; // (0,0)
+	//gl_out[gl_InvocationID].gl_Position = gl_in[1].gl_Position; // (1,0)
+	//gl_out[gl_InvocationID].gl_Position = gl_in[2].gl_Position; // (1,1)
+	//gl_out[gl_InvocationID].gl_Position = gl_in[3].gl_Position; // (0,1)
+
+	// Centres des arrêtes
+	vec4 centerOL0 = modelView * (gl_in[3].gl_Position + gl_in[0].gl_Position) / 2;
+	vec4 centerOL1 = modelView * (gl_in[1].gl_Position + gl_in[0].gl_Position) / 2;
+	vec4 centerOL2 = modelView * (gl_in[2].gl_Position + gl_in[1].gl_Position) / 2;
+	vec4 centerOL3 = modelView * (gl_in[2].gl_Position + gl_in[3].gl_Position) / 2;
+
+	// Niveau de tesselation externe par rapport aux centres
+	float tessOL0, tessOL1, tessOL2, tessOL3;
+	tessOL0 = calcTess(centerOL0);
+	tessOL1 = calcTess(centerOL1);
+	tessOL2 = calcTess(centerOL2);
+	tessOL3 = calcTess(centerOL3);
+
+	// Niveau de tesselation interne
+	float tessIL0 = max(tessOL1, tessOL3);
+	float tessIL1 = max(tessOL0, tessOL2);
+	
 
 	if (gl_InvocationID == 0) {
-		gl_TessLevelInner[0] = tessLevelInner;
-		gl_TessLevelInner[1] = tessLevelInner;
-		gl_TessLevelOuter[0] = tessLevelOuter;
-		gl_TessLevelOuter[1] = tessLevelOuter;
-		gl_TessLevelOuter[2] = tessLevelOuter;
-		gl_TessLevelOuter[3] = tessLevelOuter;
+		gl_TessLevelInner[0] = tessIL0;
+		gl_TessLevelInner[1] = tessIL1;
+		gl_TessLevelOuter[0] = tessOL0;
+		gl_TessLevelOuter[1] = tessOL1;
+		gl_TessLevelOuter[2] = tessOL2;
+		gl_TessLevelOuter[3] = tessOL3;
 	}
+	
 }
--- shaders/tessellation.tes.glsl
+++ shaders/tessellation.tes.glsl
@@ -22,7 +22,9 @@ uniform sampler2D heighmapSampler;
 vec4 interpole( vec4 v0, vec4 v1, vec4 v2, vec4 v3 )
 {
     // mix( x, y, f ) = x * (1-f) + y * f.
-    // TODO
+    vec4 v01 = mix(v0, v1, gl_TessCoord.x);
+    vec4 v23 = mix(v2, v3, gl_TessCoord.x);
+    return mix(v01, v23, gl_TessCoord.y);
 }
 
 
@@ -31,4 +33,13 @@ const float PLANE_SIZE = 256.0f;
 void main()
 {
 	// TODO
+    vec4 positionInterpolee = interpole(gl_in[0].gl_Position, gl_in[1].gl_Position, gl_in[2].gl_Position, gl_in[3].gl_Position);
+    gl_Position = mvp * positionInterpolee;
+
+    vec2 tex = ( positionInterpolee.xy / PLANE_SIZE + 0.5f) / 4;
+
+    attribOut.height = texture(heighmapSampler, tex).y * 64.0 - 32;
+    attribOut.patchDistance = vec4(gl_TessCoord, 1 - gl_TessCoord);
+    attribOut.texCoords = gl_TessCoord.xy * 2;
+    //AttribsOut.couleur = interpole(AttribsIn[0].couleur, AttribsIn[1].couleur, AttribsIn[2].couleur, AttribsIn[3].couleur);
 }
